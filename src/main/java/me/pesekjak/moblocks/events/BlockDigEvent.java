f8a4e885f5e3c92c82e29aa5adf40d86fa4723d3
jakub <jakub.zahomolkou@gmail.com>
2021-09-28 12:04:18 +0200

    Added check for custom blocks defined in config

--- src/main/java/me/pesekjak/moblocks/events/BlockDigEvent.java
+++ src/main/java/me/pesekjak/moblocks/events/BlockDigEvent.java
@@ -1,13 +1,38 @@
 package me.pesekjak.moblocks.events;
 
+import me.pesekjak.moblocks.MoBlocksPlugin;
 import me.pesekjak.moblocks.customevents.BlockDigCustomEvent;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class BlockDigEvent implements Listener {
+
+    private final Pattern dataPattern = Pattern.compile("\\{[^}]*\\}");
 
     @EventHandler
     public void onBlockDig(final BlockDigCustomEvent e) {
+
+        Block block = e.getLocation().getBlock();
+        BlockData blockdata = block.getBlockData();
+
+        String stringBlockdata = blockdata.toString();
+
+        Matcher matcher = dataPattern.matcher(stringBlockdata);
+        if (matcher.find()) {
+            stringBlockdata = matcher.group().substring(1, matcher.group().length() - 1);
+        }
+
+        if(!(MoBlocksPlugin.blocks.getConfig().get("custom blocks." + stringBlockdata + ".hardness") == null)) {
+            e.getPlayer().sendMessage(stringBlockdata + " is a custom block");
+        }
+
     }
 
 }
