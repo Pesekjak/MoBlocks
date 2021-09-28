3d7c87bd233f85abdc399c95ed19a88836565d00
jakub <jakub.zahomolkou@gmail.com>
2021-09-28 19:55:47 +0200

    Added mining fatigue effect when breaking custom blocks

--- src/main/java/me/pesekjak/moblocks/events/BlockDigEvent.java
+++ src/main/java/me/pesekjak/moblocks/events/BlockDigEvent.java
@@ -2,13 +2,16 @@ package me.pesekjak.moblocks.events;
 
 import me.pesekjak.moblocks.MoBlocksPlugin;
 import me.pesekjak.moblocks.customevents.BlockDigCustomEvent;
-import net.md_5.bungee.api.ChatColor;
-import org.bukkit.Bukkit;
+import net.minecraft.network.protocol.game.PacketPlayInBlockDig;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,6 +24,9 @@ public class BlockDigEvent implements Listener {
 
         Block block = e.getLocation().getBlock();
         BlockData blockdata = block.getBlockData();
+        PotionEffect potionEffect = new PotionEffect(PotionEffectType.SLOW_DIGGING, 99999, -1, false, false, false);
+        PotionEffect[] potionArray = {potionEffect};
+        Collection<PotionEffect> potionCollection = Arrays.asList(potionArray);
 
         String stringBlockdata = blockdata.toString();
 
@@ -30,7 +36,12 @@ public class BlockDigEvent implements Listener {
         }
 
         if(!(MoBlocksPlugin.blocks.getConfig().get("custom blocks." + stringBlockdata + ".hardness") == null)) {
-            e.getPlayer().sendMessage(stringBlockdata + " is a custom block");
+            if(e.getAction() == PacketPlayInBlockDig.EnumPlayerDigType.a) {
+                e.getPlayer().addPotionEffects(potionCollection);
+            }
+            else if (e.getAction() == PacketPlayInBlockDig.EnumPlayerDigType.b) {
+                e.getPlayer().removePotionEffect(PotionEffectType.SLOW_DIGGING);
+            }
         }
 
     }
